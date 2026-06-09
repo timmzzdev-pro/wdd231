77a36be82dbcb1bddc1d69540086182a7ed44d40
timmzzdev-progit config --global user.email tolajide1@byupathway.edu <timilehinolajide8464@gmail.com>
2026-06-09 11:57:30 +0100

    title fixed

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -3,3 +3,17 @@ const timestamp = document.querySelector("#timestamp");
 if (timestamp) {
 timestamp.value = new Date().toISOString();
 }
+document.querySelectorAll(".open-modal").forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const modalId = link.dataset.modal;
+        document.getElementById(modalId).showModal();
+    });
+});
+
+document.querySelectorAll(".close-modal").forEach(button => {
+    button.addEventListener("click", () => {
+        button.closest("dialog").close();
+    });
+});
